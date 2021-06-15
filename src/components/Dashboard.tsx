90b590517a50cdf330223ca3b7d0df6233af8731
Vincenzo Mann <vinnymann97@gmail.com>
2021-06-15 19:37:33 +0100

    Structure initial files

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
+
+interface Props extends RouteComponentProps {
+
+}
+
+const Dashboard: React.FC<Props> = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default Dashboard;
